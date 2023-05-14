680925cf667997fe2d339dc10998b4371116d438
Takhirjan <takhirjon22!@mail.ru>
2023-05-14 10:30:30 +0600

    thymleaf decorate

--- src/main/java/springboot/techboot/controller/Homecontroller.java
+++ src/main/java/springboot/techboot/controller/Homecontroller.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import springboot.techboot.DB.Music;
@@ -14,45 +15,56 @@ import java.util.ArrayList;
 public class Homecontroller {
 
   @GetMapping(value = "/") // @WebServlet(value = "/") + doGet()
-  public String indexPage(Model model){
+  public String indexPage(Model model) {
     ArrayList<Music> musicArray = DBManager.getMusics();
     model.addAttribute("musics", musicArray);
     // request.setAttribute("muzikalar", musicArray);
     return "index"; // request.getRequestDispatchet("/index.html").forward(request, response);
   }
 
-  @GetMapping(value="/about")
-  public String about(){
+  @GetMapping(value = "/about")
+  public String about() {
     return "about_page";
   }
+
   @PostMapping(value = "/add-music")
-  public String addMusic(Music music){
-  DBManager.addMusic(music);
-  return "redirect:/";
+  public String addMusic(Music music) {
+    DBManager.addMusic(music);
+    return "redirect:/";
   }
+
   @PostMapping(value = "/add-music-v2")
   public String addMusicV2(
-      @RequestParam(name="music-name") String name,
-      @RequestParam(name="music-author") String author,
-      @RequestParam(name="music-duaration") int duration
-  ){
-  Music music=new Music();
+      @RequestParam(name = "music-name") String name,
+      @RequestParam(name = "music-author") String author,
+      @RequestParam(name = "music-duaration") int duration
+  ) {
+    Music music = new Music();
     music.setName(name);
     music.setAuthor(author);
     music.setDuaration(duration);
     DBManager.addMusic(music);
     return "redirect:/";
   }
+
   @GetMapping(value = "/music-details")
-  public String getMusic(@RequestParam(name = "MusicId")int id,
-                         Model model){
-    Music music=DBManager.getMusic(id);
-    model.addAttribute("muzyka",music);
-    return  "details";
+  public String getMusic(@RequestParam(name = "MusicId") int id,
+                         Model model) {  //Толькочерех MODel мы можем передать объект через атрибуту
+    Music music = DBManager.getMusic(id);
+    model.addAttribute("muzyka", music);
+    return "details";
 
   }
+
   @GetMapping(value = "/add-music")
-  public String addMudicPage(Model model){
+  public String addMudicPage(Model model) {
     return "addmusic";
+  }
+
+  @GetMapping(value = "/details/{musicId}")
+  public String musicDetails(@PathVariable(name = "musicId") int id, Model model) {
+    Music music = DBManager.getMusic(id);
+    model.addAttribute("muzyka", music);
+    return "details";
   }
 }
