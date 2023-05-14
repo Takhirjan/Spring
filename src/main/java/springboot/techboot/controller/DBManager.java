6bbade596a64ca8c6d500674560e7dccf88bc06b
Takhirjan <takhirjon22!@mail.ru>
2023-05-14 11:23:20 +0600

    th:style

--- src/main/java/springboot/techboot/controller/DBManager.java
+++ src/main/java/springboot/techboot/controller/DBManager.java
@@ -7,12 +7,18 @@ import java.util.ArrayList;
 public class DBManager {
   private static final ArrayList<Music> musics=new ArrayList<>(); //final щзначает, что его я не могу переназначать,
   // но менять внутри можно
-  private static Long id=5L;
+  private static Long id=10L;
   static {
     musics.add(new Music(1L,"Music 1","Singer 1",188));
     musics.add(new Music(2L,"Music 2","Singer 2",155));
     musics.add(new Music(3L,"Music 3","Singer 3",157));
     musics.add(new Music(4L,"Music 4","Singer 4",160));
+    musics.add(new Music(5L,"Music 5","Singer 5",200));
+    musics.add(new Music(6L,"Music 6","Singer 6",189));
+    musics.add(new Music(7L,"Music 7","Singer 7",320));
+    musics.add(new Music(8L,"Music 8","Singer 8",154));
+    musics.add(new Music(9L,"Music 9","Singer 9",202));
+
   }
   public static ArrayList<Music> getMusics(){
     return musics;
